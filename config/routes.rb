746d175a08cc47dd55115fe00e2275b4612aa873
Kevin Wang <kevin0117@gmail.com>
2020-10-11 20:20:19 +0800

    Set up routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'users#new'
   resources :users, except: [:new]
 
+  # Sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root "bootstrap#index"
 end
