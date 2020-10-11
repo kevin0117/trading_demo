490ba0998797c1800b818b611bf4b0e63887a22b
Kevin Wang <kevin0117@gmail.com>
2020-10-11 23:54:23 +0800

    Issue resolved: User try to close a dean when that deal is actually cancelled by the owner

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -1,5 +1,5 @@
 class CommoditiesController < ApplicationController
-  before_action :find_commodity, only: %i[edit show update destroy deal]
+  before_action :find_commodity, only: %i[edit show update destroy]
   
   def index
     @commodities = Commodity.order(id: :desc)
@@ -40,11 +40,14 @@ class CommoditiesController < ApplicationController
   end
 
   def deal
-    @commodity.trade!
-    @commodity.closer_id = current_user.id
-    @commodity.save
-
-    redirect_to commodities_path, notice: "下單成功"
+    @commodity = Commodity.find_by(id: params[:id])
+    if @commodity && @commodity.trade!
+      @commodity.closer_id = current_user.id
+      @commodity.save
+      redirect_to commodities_path, notice: "下單成功"
+    else
+      redirect_to commodities_path, notice: "下單失敗"
+    end
   end
 
   def edit; end
