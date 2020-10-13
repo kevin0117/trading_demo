566d2ff6bc9602c75ec59e6f711b145acbb04990
Kevin Wang <kevin0117@gmail.com>
2020-10-13 17:12:33 +0800

    Procfile and Mailer Configuation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['smtp_address'],
+    port: 587,
+    domain: 'postmaster@chienhao.tw',
+    user_name: ENV['smtp_username'],
+    password: ENV['smtp_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
