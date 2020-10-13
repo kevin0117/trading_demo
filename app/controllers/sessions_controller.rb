1366834207335e9f0af3a91d47a5b828d4002b01
Kevin Wang <kevin0117@gmail.com>
2020-10-13 09:41:32 +0800

    add time limit control and cable configuration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,11 @@ class SessionsController < ApplicationController
   def create
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
+      cookies.signed['user.id'] = @user.id
+      # 登入系統 60分鐘後， 交易平台的頻道就會失去連接
+      cookies.signed['user.expires_at'] = 60.minutes.from_now
       flash[:notice] = '成功登入'
-      redirect_to users_path(@user)
+      redirect_to commodities_path(@user)
     else
       if params[:session][:email] == ''
         redirect_to login_path, notice: 'Email 不可空白'
@@ -21,6 +24,8 @@ class SessionsController < ApplicationController
   # 使用者登出
   def destroy
     session[:user_id] = nil
+    cookies.signed['user.id'] = nil
+    cookies.signed['user.expires_at'] = nil
     redirect_to root_path, notice: '已登出'
   end
 
